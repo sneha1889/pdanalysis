f70ae4b37ad1e56f090bc7ec5adbdaf0cb993014
Nitin Deshpande <deshpand@in.tum.de>
2015-03-10 22:48:16 +0100

    My first github commit

---
+++ thresholdvoltage.m
@@ -0,0 +1,14 @@
+function thresholdRowValue = thresholdvoltage(frequencyPD, row_offset) 
+%% Gives the row value where threshold voltage is found
+counter = 1;
+rows = size(frequencyPD);
+for i = 1:rows
+    if frequencyPD(i)== 1
+        thresholdRowValue = counter + row_offset;
+        break
+    else 
+         counter =counter+1;
+    end
+end
+end
+ 
