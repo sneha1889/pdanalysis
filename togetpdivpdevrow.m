510297d06680335de8a6c34ba9a25321cfda9230
sneha1889 <snehalatharkv@gmail.com>
2015-05-04 01:58:11 +0200

    Changed pdiv and pdev row and xls write

--- togetpdivpdevrow.m
+++ togetpdivpdevrow.m
@@ -1,33 +1,62 @@
-function [startrowList,endrowList,pdivRow,pdevRow] = togetpdivpdevrow(thresholdRowValue,frequencyPD, row_offset) 
+function [startrowList,endrowList,pdivRow,pdevRow,sizeList] = togetpdivpdevrow(thresholdRowValue,frequencyPD, row_offset) 
 %% Gives the row value where pdiv and pdev voltage is found
-n = 0;
+%n = -1;
 counter=0;
+indexMax=0;
 endrowList = [0];
 startrowList =[0];
-index = 0;
+indexpdSum = 0;
+indexsizeList =0;
 sizeList = [0];
+pdSum =[0];
 rows = size(frequencyPD);
 indexMax=0;
+thresholdRowValue=thresholdRowValue-1;
+cycle = 0;
+temA=0;
+pdevRowValue=0;
 
-for i = thresholdRowValue:rows
-    if frequencyPD(i)>= 1
-        n=i;
-        counter=counter+1;
-        if (frequencyPD(n)>=1 && frequencyPD(n+1) == 0)
-            pdevRowValue = n;
-            index = index+1;
-            sizeList(index)= counter;
-            % stores each of the ending row number of the range. The ending
-            % whenever the frequencyPD(n) has a zero in it. 
-            endrowList(index) = pdevRowValue;
-            thresholdRowValue = pdevRowValue;
-            continue;
-        end
-    end
+if mod(thresholdRowValue,2) == 0
+    cycle = -1;
+    n=thresholdRowValue+1;
+else
+    cycle = 1; 
+    n=thresholdRowValue;
 end
-sizeList = cat(2,sizeList(1),abs(diff(sizeList)));
+
+for i = n:rows
+    if (i~=size(frequencyPD))
+        temA=frequencyPD(i)+frequencyPD(i+1);
+       % disp('n is 1999')
+    else
+        temA=frequencyPD(i);
+       % disp('n is not 1999')
+    end
+    if (temA~=0)
+       indexpdSum=indexpdSum+1;
+       pdSum(indexpdSum)=temA;
+       counter=counter+1;
+       n=i+2;
+    else
+       indexsizeList=indexsizeList+1;
+       sizeList(indexsizeList)=counter;
+       counter=0;
+       if (frequencyPD(i-1)> 0)
+            pdevRowValue=i-1;
+            n=pdevRowValue+2;
+       else
+            pdevRowValue=i-2;
+            n=pdevRowValue+3;
+       end
+       endrowList(indexsizeList)=pdevRowValue;
+      %fprintf('the end rows are:%d\n',endrowList);
+    end
+    %n=i+2;
+end %end for
+%sizeList = cat(2,sizeList(1),abs(diff(sizeList)));
+%fprintf('the rows are:%d\n', sizeList);
 startrowList = endrowList-sizeList;
 [M,indexMax] = max(sizeList(:));
-pdivRow = startrowList(indexMax)+ 1 + row_offset;
+pdivRow = startrowList(indexMax)+2+ row_offset;
 pdevRow = endrowList(indexMax)+ row_offset;
 end
--- writexlsfile.m
+++ writexlsfile.m
@@ -1,6 +1,6 @@
 function writexlsfile(fileName,thresholdRowValue,thresholdVoltage,pdivRow,pdivVoltage,pdevRow,pdevVoltage)
-toWrite = {' ','ROW NUMBER','VOLTAGE (V)';'THRESHOLD',thresholdRowValue,thresholdVoltage;'PDIV',pdivRow,pdivVoltage;'PDEV',pdevRow,pdevVoltage};
-xlswrite(fileName,toWrite,'Daten','Z8');
+toWrite = {'Result Ver.2 ','ROW NUMBER','VOLTAGE (V)';'THRESHOLD',thresholdRowValue,thresholdVoltage;'PDIV',pdivRow,pdivVoltage;'PDEV',pdevRow,pdevVoltage};
+xlswrite(fileName,toWrite,'Daten','Z13');
 fprintf('Done writing file %s',fileName);
 end
 
